87ed9937aa3e3e19dcf5880f1e3181043f4f93b5
veerarongali <veerarongali@gmail.com>
2025-09-22 07:58:16 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+Ramesh:: Hi Suresh, how are you?
+Suresh:: Hi Ramesh, i am fine. how about you?
+Ramesh:: I am also fine Suresh, what are you learning presently?
+Suresh:: I am learning shell scripting Ramesh.
