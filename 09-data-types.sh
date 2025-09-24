c2ec5dc821599fd2ae470826ab12bf32bf5ae3ab
veerarongali <veerarongali@gmail.com>
2025-09-24 09:28:47 +0530

    special variables

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+NUMBER1=100
+NUMBER2=200
+NAME=DevOps
+
+SUM = $ (($NUMBER1+$NUMBER2+$NAME))
+
+echo "SUM is: ${SUM}"
+
+# Size = 4, Max index = 3
+LEADERS=("Modi" "Putin" "Trudo" "Trump")
+
+echo "All leaders: ${LEADERS[@]}"
+echo "First leader: ${LEADER[0]}"
+echo "First leader: ${LEADER[10]}"
---
+++ 10-conditions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -lt 10 ]; then
+    echo "Given number $NUMBER is less than 10"
+
+elif [ $NUMBER -eq 10 ]; then
+    echo "Given number $NUMBER is equal to 10"
+
+else
+    echo "Given number $NUMBER is greater than 10"
+
+fi
+
+
+#-lt =lessthan
+#-gt =greaterthan
+#-eq =equal to
